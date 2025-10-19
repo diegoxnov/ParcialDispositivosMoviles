c871cb137881931dfa0a760b3c459dcea865556e
Diego Nova <dnovar@ulasalle.edu.pe>
2025-10-19 13:01:44 -0500

    Agregando y configurando el welcome fragment

---
+++ ParcialJuego/app/src/main/java/com/example/parcialjuego/WelcomeFragment.kt
@@ -0,0 +1,49 @@
+package com.example.parcialjuego
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Referencias a los botones del layout
+        val btnMostrarReglas = view.findViewById<Button>(R.id.btnMostrarReglas)
+        val btnIniciarJuego = view.findViewById<Button>(R.id.btnIniciarJuego)
+
+        // Botón para mostrar reglas
+        btnMostrarReglas.setOnClickListener {
+            mostrarReglas()
+        }
+
+        // Botón para ir  al juego
+        btnIniciarJuego.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_gameFragment)
+        }
+    }
+
+    private fun mostrarReglas() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Reglas del juego")
+            .setMessage(
+                """
+                - Encuentra todas las parejas antes de que se acabe el tiempo.
+                - Cada acierto suma puntos.
+                - Cada error resta segundos del reloj.
+                - ¡Supera tu récord personal!
+                """.trimIndent()
+            )
+            .setPositiveButton("Entendido") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+}
